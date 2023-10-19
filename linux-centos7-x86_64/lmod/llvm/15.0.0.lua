96bef8a99b9758d66d20914680131f304516ef11
TaufeqRazakh <taufeqmdr@gmail.com>
2023-10-19 00:14:35 -0700

    Lua scripts to load llvm 15.0.0

---
+++ linux-centos7-x86_64/lmod/llvm/15.0.0.lua
@@ -0,0 +1,13 @@
+whatis("Name: llvm")
+whatis("Version: 15.0.0")
+whatis("Target: x86_64")
+whatis("Category: compiler")
+whatis("Keywords: compiler, clang++, clang, lldb")
+whatis("Short description: LLVM project including x86 targets")
+whatis("URL: https://llvm.org")
+
+prepend_path("PATH", "/project/anakano_81/razakh/linux-centos7-x86_64/llvm/main-20220712/bin")
+prepend_path("LD_LIBRARY_PATH", "/project/anakano_81/razakh/linux-centos7-x86_64/llvm/main-20220712/lib")
+
+
+
